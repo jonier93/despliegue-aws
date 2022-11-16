67571f53b9131b0744c05c36d86979c316e750c0
jonier93 <jonietrporras@gmail.com>
2022-11-16 18:27:54 -0500

    version1

---
+++ server.js
@@ -0,0 +1,16 @@
+import ex from 'express'
+import cors from 'cors'
+import {router} from './routes/route.js'
+
+let app = ex()
+
+app.use(cors())
+app.use(ex.json())
+app.use(ex.static("src/build"))
+app.use(router)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, function(){
+    console.log("Servidor iniciado")
+})
