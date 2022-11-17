d95ca6eae2fec6dbb7ccd7177a0bad79adc20644
jonier93 <jonietrporras@gmail.com>
2022-11-16 19:18:27 -0500

    version2

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(ex.json())
 app.use(ex.static("src/build"))
 app.use(router)
 
+app.use(function(req, res) {
+    res.redirect('/')
+});
+
 const port = process.env.PORT || 5000
 
 app.listen(port, function(){
